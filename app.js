aa31f1dc98b1fc5b332e0f62939040211b8685ae
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-13 19:31:21 -0500

    finish the first try

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
 const agent = document.querySelector(".agent img");
-const link = document.querySelector(".link button");
+const link = document.querySelector(".link");
 const description = document.querySelector(".info h3");
 const abilities = document.querySelector(".abilities");
 
@@ -14,24 +14,29 @@ const abilities = document.querySelector(".abilities");
 container.addEventListener("mousemove", (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateY(${yAxis}deg)`;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 })
 
 // Animate In
-container.addEventListener("mouseenter", e => {
+container.addEventListener("mouseenter", (e) => {
     card.style.transition = "none";
     //Popout effect
     title.style.transform = "translateZ(150px)";
-    agent.style.transform = "translateZ(150px)";
-    link.style.transform = "translateZ(150px)";
-    description.style.transform = "translateZ(150px)";
-    abilities.style.transform = "translateZ(150px)";
+    // agent.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    agent.style.transform = "translateZ(200px)";
+    description.style.transform = "translateZ(125px)";
+    abilities.style.transform = "translateZ(100px)";
+    link.style.transform = "translateZ(75px)";
 });
 
 // Animate Out
-container.addEventListener("mouseleave", e => {
+container.addEventListener("mouseleave", (e) => {
     card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateY(0deg)`;
-    //Popout effect
+    //Popback effect
     title.style.transform = "translateZ(0px)";
+    agent.style.transform = "translateZ(0px)";
+    description.style.transform = "translateZ(0px)";
+    abilities.style.transform = "translateZ(0px)";
+    link.style.transform = "translateZ(0px)";
 });
